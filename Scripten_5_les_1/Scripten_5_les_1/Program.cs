be15563887ffbdb1688b4cd24c880ee2ae634f72
nimaef <SintLucasNimaef@users.noreply.github.com>
2021-03-17 11:06:28 +0100

    EnumOpdracht
    dit was de opdrachtr en ik heb per ongeluk part 2 en 3 omgedraaid.
    
    Denk dat ik het op deze manier moet inleveren maar heb geen idee.

---
+++ Scripten_5_les_1/Scripten_5_les_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Scripten_5_les_1
+{
+    class Program
+    {
+        // part 1
+        enum directions { north, south, east, west }
+
+        
+
+        static void Main(string[] args)
+        {
+            enumber();
+
+            void enumber()
+            {
+                //part 2
+                int ChosenDirection = 0;
+
+                Console.WriteLine("Choose a direction");
+                Console.WriteLine("North, South, East , West (0, 1, 2, 3) ");
+                ChosenDirection = int.Parse(Console.ReadLine());
+
+                if (ChosenDirection < 0 | ChosenDirection > 3)
+                {
+                    enumber();
+                }
+                else
+                {
+                    var playerDirection = (directions)ChosenDirection;
+                    Console.WriteLine((directions)ChosenDirection);
+
+                    Console.WriteLine("Press enter to continue...");
+
+                    Console.ReadLine();
+
+                    Console.Clear();
+
+                    enumber2();
+                }
+                
+                void enumber2()
+                {
+                    string DirectionChosen;
+
+                    Console.WriteLine("Choose a direction");
+                    Console.WriteLine("North, South, East, West");
+
+                    DirectionChosen = Console.ReadLine();
+
+                    
+                    DirectionChosen = Console.ReadLine();
+
+                    if (DirectionChosen == directions.north.ToString())
+                    {
+                        Console.WriteLine("North");
+                    }else if (DirectionChosen == directions.north.ToString())
+                    {
+                        Console.WriteLine("South");
+                    }else if (DirectionChosen == directions.north.ToString())
+                    {
+                        Console.WriteLine("East");
+                    }else if (DirectionChosen == directions.north.ToString())
+                    {
+                        Console.WriteLine("West");
+                    }
+
+                }
+            }
+
+        }
+    }
+}
